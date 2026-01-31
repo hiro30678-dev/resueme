292ab47edf120bff868a18651f37d41d81f08e6c
hiro30678-dev <hiro30678@gmail.com>
2026-01-31 08:45:21 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ export const getAsset = (path: string) => {
 const siteConfig = {
   personal: {
     name: 'Zayn',
-    title: 'Your Title | Your Role',
+    title: 'boxer | Your Role',
     image: getAsset('images/profile.png'), // Customize or replace with your profile image
     description:
       'Write a short bio here describing your background and focus. Keep it to 2–4 sentences.\n' +
